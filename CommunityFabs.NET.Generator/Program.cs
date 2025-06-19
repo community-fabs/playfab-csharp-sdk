9522397dfc4003753c019b483062a3b7447622bb
Greg Willard (r3pwn) <wickett06@gmail.com>
2025-06-18 23:09:21 -0400

    feat: parallelize SDK document retrieval

--- CommunityFabs.NET.Generator/Program.cs
+++ CommunityFabs.NET.Generator/Program.cs
@@ -11,16 +11,23 @@ Utils.RecursiveCopy(staticPath, outPath);
 var toc = await ApiDetails.GetTableOfContents();
 var combinedSdkApiRefs = toc.Documents!
     .Where(docRef => docRef.SdkGenMakeMethods != null && docRef.SdkGenMakeMethods.Contains("makeCombinedAPI"));
-Console.WriteLine("Fetched Table of Contents with {0} documents to process", toc.Documents!.Count);
+Console.WriteLine("Fetched Table of Contents with {0} documents to process", combinedSdkApiRefs.Count());
 
-foreach (var docReference in combinedSdkApiRefs)
+var apiData = await Task.WhenAll(combinedSdkApiRefs.Select(docRef => ApiDetails.GetLegacyDocument(docRef.RelPath!)));
+
+foreach (var apiDoc in apiData)
 {
-    var apiDoc = await ApiDetails.GetLegacyDocument(docReference.RelPath!);
-    Console.WriteLine("Fetched {0} with {1} methods and {2} data types", docReference.RelPath!, apiDoc.Calls!.Count, apiDoc.Datatypes!.Count);
+    Console.WriteLine("Processing PlayFab{0}Api with {1} methods and {2} data types", apiDoc.Name, apiDoc.Calls!.Count, apiDoc.Datatypes!.Count);
 
     var modelsFileName = $"PlayFab{apiDoc.Name}ApiModels";
-    var rendered = await RazorTemplateEngine.RenderAsync("/Views/Models/ApiModel.cshtml", apiDoc);
+    var renderedModel = await RazorTemplateEngine.RenderAsync("/Views/Models/ApiModel.cshtml", apiDoc);
 
-    var filePath = Path.Combine(outPath, "Models", $"{modelsFileName}.cs");
-    await File.WriteAllTextAsync(filePath, rendered);
+    var modelFilePath = Path.Combine(outPath, "Models", $"{modelsFileName}.cs");
+    await File.WriteAllTextAsync(modelFilePath, renderedModel);
+
+    var interfaceFileName = $"IPlayFab{apiDoc.Name}Api";
+    var renderedInterface = await RazorTemplateEngine.RenderAsync("/Views/Interface.cshtml", apiDoc);
+
+    var interfaceFilePath = Path.Combine(outPath, "Interfaces", $"{interfaceFileName}.cs");
+    await File.WriteAllTextAsync(interfaceFilePath, renderedInterface);
 }
