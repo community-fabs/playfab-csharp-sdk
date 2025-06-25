7dfbc45505af6226090c29ce2168fbecacd56977
Greg Willard (r3pwn) <wickett06@gmail.com>
2025-06-25 00:41:25 -0400

    feat: more closely mimic the official SDK (for now)

--- CommunityFabs.NET.Generator.Templates/Static/Utils/HttpHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CommunityFabs.NET.Sdk.Utils;
-
-public static class HttpHelper
-{
-    // Static, shared HttpClient instance
-    private static readonly HttpClient httpClient = new();
-
-    public static HttpClient HttpClient { get => httpClient; }
-}
